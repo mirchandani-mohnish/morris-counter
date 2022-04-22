6b306459d34ebf8e8eaee87702ffd161cceb8322
Mohnish Mirchandani <mirchandani.mohnish03@gmail.com>
2022-04-22 11:51:35 +0530

    Update: Plots generated

--- plot_morriscounter.py
+++ plot_morriscounter.py
@@ -28,7 +28,7 @@ from morriscounter import MorrisCounter
 * Generate plots based on the arrays formed 
 '''
 
-def test(counter: MorrisCounter, n: int, axis: Axis) -> None:
+def test(counter: MorrisCounter, n: int, graphNo:int) -> None:
   uppers, lowers, estimates, actuals = [], [], [], []
   for N in range(1, n+1): 
     counter.increment() 
@@ -49,12 +49,14 @@ def test(counter: MorrisCounter, n: int, axis: Axis) -> None:
   # Create the graphs based on the four arrays. 
   MARKER_SIZE=.01
   LINE_WIDTH=.5
-  axis.plot(actuals, estimates, label="estimate", marker="o", color="green", markersize=MARKER_SIZE, linewidth=LINE_WIDTH)
-  axis.plot(actuals, lowers, label="lower bound", color="red", linestyle="dashed", markersize=MARKER_SIZE, linewidth=LINE_WIDTH)
-  axis.plot(actuals, uppers, label="upper bound", color="red", linestyle="dashed", markersize=MARKER_SIZE, linewidth=LINE_WIDTH)
-  axis.plot(actuals, actuals, label="actual", linestyle="dashed", color="grey", markersize=MARKER_SIZE, linewidth=LINE_WIDTH, alpha=0.3)
-  axis.set_title(rf"$\varepsilon={counter.epsilon:.3g}$, $\delta={counter.delta:.3g}$", fontsize=6, y=-0.2)
-  axis.axis('off')
+  plt.plot(actuals, estimates, label="estimate", marker="o", color="green", markersize=MARKER_SIZE, linewidth=LINE_WIDTH)
+  plt.plot(actuals, lowers, label="lower bound", color="red", linestyle="dashed", markersize=MARKER_SIZE, linewidth=LINE_WIDTH)
+  plt.plot(actuals, uppers, label="upper bound", color="red", linestyle="dashed", markersize=MARKER_SIZE, linewidth=LINE_WIDTH)
+  plt.plot(actuals, actuals, label="actual", linestyle="dashed", color="grey", markersize=MARKER_SIZE, linewidth=LINE_WIDTH, alpha=0.3)
+  # plt.show()
+  plt.savefig('plots/fig'+str(graphNo)+'.png', dpi=1000)
+  plt.cla()
+  
 
 
 
@@ -76,22 +78,16 @@ def test(counter: MorrisCounter, n: int, axis: Axis) -> None:
 
 if __name__ == '__main__':
   n = 100
-  bins = 10
-  fig, axes = plt.subplots(bins, bins)
+  bins = 4
+  graphNo = 1
+  # fig, axes = plt.subplots(bins, bins)
   print(np.linspace(.5, .95, bins))
   for r, accuracy in enumerate(np.linspace(.5, .95, bins)): # Create an array of evenly distributed numbers between 0.5 and 0.95 
     for c, max_failure_rate in enumerate(np.linspace(0.05, .45, bins)): 
       print(f"{accuracy=}, {max_failure_rate=}") 
       counter = MorrisCounter(accuracy=accuracy, max_failure_rate=max_failure_rate) # Iterate across each array and make a counter instance
-      test(counter, n=n, axis=axes[r][c]) # Pass the counter instance to the test function. 
-      
-
-
-  # Create and Save the graph
-  fig.suptitle(f"Morris(a) Counter, {n=}")
-  handles, labels = axes[-1][-1].get_legend_handles_labels()
-  fig.legend(handles, labels, loc='upper left', prop={'size': 6})
-  fig.savefig("plots/morris-a.png", dpi=500)
+      test(counter, n=n, graphNo=graphNo) # Pass the counter instance to the test function. 
+      graphNo += 1
 
 
 
