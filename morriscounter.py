ab05eb14a8536505b33c2db5f3246becf331bcab
Mohnish Mirchandani <mirchandani.mohnish03@gmail.com>
2022-04-22 06:17:01 +0530

    Update: Analysis and Commenting Added

--- morriscounter.py
+++ morriscounter.py
@@ -1,8 +1,8 @@
 from random import Random
 from typing import Optional
 
-class MorrisCounter:
-  """An approximate counter.
+"""
+~~~An approximate counter~~~
 
   This is the Morris(a) counter.
 
@@ -15,7 +15,45 @@ class MorrisCounter:
     * the amount of space used is O(lglgN+lg(1/e)+lg(1/d)) with prob <= N*(1+a)^(-Z), where Z = (lgN/a)^C
     * the counter estimate N' satisfies Pr[|N-N'| > e*N] < d
 
+  Analysis
+    Group: 2
+      * Priya Jani AU2040004
+      * Yansi Memdani AU2040028
+      * Abhishu Oza AU2040027
+      * Priyanshu Pathak AU2040241
+      * Mohnish Mirchandani AU2040110
   """
+
+'''
+  MorrisCounter
+  * Counter Class with five member variables and three member methods 
+
+    - Variables
+      + X:- Number of Counts in the Counter
+      + epsilon:- The Maximum possible error
+      + delta:- The Failure Rate
+      + a:- 
+      + rng:- A Random Number
+
+    - Functions: 
+      + count:- Returns the current count in the morris counter
+      + increment:- Increments the counter
+      + _should_increment:- Checks and computes whether the counter should be incremented based on thhe increment_probability
+      + _increment_probability:- Returns the probability with which the counter should be incremented.  
+
+
+  * Program Flow 
+    - Count Function returns the current approximate count where count = (1/a)*((1+a)^(X) - 1)
+    - Increment Function Probabilistically checks whether the counter should be incremented.
+      + Call to _should_increment => which returns Bool
+      + _should_increment compares a random number with the increment probability and if 
+        > Random Number less than Probability => Return False
+        > Random Number greater than Probability => Return True
+  '''
+
+
+
+class MorrisCounter:
   X: float
   epsilon: float
   delta: float
@@ -25,7 +63,6 @@ class MorrisCounter:
   def __init__(self, *, accuracy: float, max_failure_rate: float, rng: Optional[Random] = None) -> None:
     assert 1/2 <= accuracy < 1, accuracy
     assert 0 < max_failure_rate < 1/2, max_failure_rate
-
     self.epsilon = 1 - accuracy
     self.delta = max_failure_rate
     self.a = 2 * self.epsilon ** 2 * self.delta
@@ -35,16 +72,28 @@ class MorrisCounter:
 
     print("epsilon: ", self.epsilon, "    delta", self.delta, "    a", self.a)
 
+  '''
+  Increment
+   - Check whether the counter should be incremented and increment it accordingly
+  '''
+
+
   def increment(self) -> None:
     if self._should_increment:
       self.X += 1
+  '''
+  Count 
+   - Return the currently estimated count in the counter. 
+  '''
 
   @property
   def count(self) -> float:
     return self.a ** (-1) * ((1 + self.a) ** self.X - 1)
-  '''
-  Count 
 
+
+  '''
+  _increment_probability
+    - Return the current probability of increment of counter.
   '''
 
 
@@ -53,34 +102,14 @@ class MorrisCounter:
     return (1 + self.a) ** (-self.X)
 
   '''
-  
+  _should_increment
+  Check whether the counter should be incremented by:
+    - Generate a Random Number from a Uniform distribution
+    - Compare it with the current increment_probability
+  Return True or False respectively
   '''
   @property
   def _should_increment(self) -> bool:
     return self.rng.uniform(0, 1) <= self._increment_probability
-    # The uniform function generates a uniform probability distribution i.e. any value between 0 and 1 is equally likely
-
-
-  '''
-  Working of the Current Morris Counter
-    When the count function is called, it returns the count based on the values of x and a;
-
-    The increment function acts as a trigger for the two other functions, namely "should_increment" and "increment_probability"
-    When you call the increment function -> it checks whether it should increment. 
-    The "should_increment" selects a value based on a uniform distribution 
-    The "increment_probability" function gives the value of the probability #########################
-    After the comparison , a bool is returned for whether the counter should increment or not
-
-  '''
-
-
-
-
-  '''
-  Todo: 
-  Add an example 
-  Format the comments
+    
   
-  
-  
-  '''
--- plot_morriscounter.py
+++ plot_morriscounter.py
@@ -5,23 +5,39 @@ from matplotlib.axis import Axis
 from morriscounter import MorrisCounter
 
 '''
-The following test file is only responsible for creating the graphs which are found
-We define "uppers, lowers, estimates, actuals" and keep appending the respective values of the counter 
-upon each loop, into the respective arrays. 
-
-This gets an array for each field. 
-
-This is then consecutively plotted on the chart. 
+~~ plot_morriscounter.py ~~
+  * Responsible for creating the graphs and testing the Morris Counter
+  * Bounds defined within the test function 
+    - uppers :- The Upper bounds 
+    - lowers :- The Lower bounds
+    - estimates :- The estimated count
+    - actuals :- The aactual number of values
+  * These store the respective values of the counter
+  * These values are then plotted and stored as a png file. 
 '''
+
+
+
+'''
+~~ Test Function ~~
+* Program Flow 
+  - For every value from 1 to N: 
+    - increment the counter
+    - check all four values 
+    - append the values in their respective arrays
+* Generate plots based on the arrays formed 
+'''
+
 def test(counter: MorrisCounter, n: int, axis: Axis) -> None:
   uppers, lowers, estimates, actuals = [], [], [], []
-  for N in range(1, n+1):
-    counter.increment()
-    # assert (1-counter.epsilon)*N <= counter.count <= (1+counter.epsilon)*N
-    actual = N
-    estimate = counter.count
-    lower = (1-counter.epsilon)*N
+  for N in range(1, n+1): 
+    counter.increment() 
+    # Counter Incremented
+    actual = N # The actual number which should be displayed 
+    estimate = counter.count # Count being stored in the counter. 
+    lower = (1-counter.epsilon)*N 
     upper = (1+counter.epsilon)*N
+
 
     actuals.append(actual)
     estimates.append(estimate)
@@ -30,7 +46,7 @@ def test(counter: MorrisCounter, n: int, axis: Axis) -> None:
 
     # print(f"{actual=}, {estimate=:.8f} in [{lower:.8f}, {upper:.8f}]")
 
-  # The following portion is responsible for the creation of the respective graphs we see.
+  # Create the graphs based on the four arrays. 
   MARKER_SIZE=.01
   LINE_WIDTH=.5
   axis.plot(actuals, estimates, label="estimate", marker="o", color="green", markersize=MARKER_SIZE, linewidth=LINE_WIDTH)
@@ -42,20 +58,36 @@ def test(counter: MorrisCounter, n: int, axis: Axis) -> None:
 
 
 
+
+'''
+~~ Main Function ~~
+* Respective inputs are taken 
+  - accuracy: 
+  - max_failure_rate:
+  - bins: 
+* An array is created to mark values based on the number of bins we create. 
+  - The accuracy is restricted between 0.5 and 0.95, we create an evenly distributed array of length (bins) across the range [0.5, 0.95)
+  - The max_failure_rate is restricted between 0.05 and 0.55, we create an evenly distributed array of length (bins) across the range [0.05, 0.45)
+* A counter instance is called over each corresponding value of accuracy and max_failuire_rate
+* This instance is passed to the test function for generating the graph and testing bounds
+* The graphs are finally saved in the png file. 
+'''
+
+
 if __name__ == '__main__':
   n = 100
   bins = 10
   fig, axes = plt.subplots(bins, bins)
   print(np.linspace(.5, .95, bins))
-  for r, accuracy in enumerate(np.linspace(.5, .95, bins)): #np.linspace creates an array of numbers between 0.5 and 0.95 where the array length is "bins" and the array is evenly distributed
-    for c, max_failure_rate in enumerate(np.linspace(0.05, .45, bins)): # r and c work with the number of elements in the respective arrays
+  for r, accuracy in enumerate(np.linspace(.5, .95, bins)): # Create an array of evenly distributed numbers between 0.5 and 0.95 
+    for c, max_failure_rate in enumerate(np.linspace(0.05, .45, bins)): 
       print(f"{accuracy=}, {max_failure_rate=}") 
-      counter = MorrisCounter(accuracy=accuracy, max_failure_rate=max_failure_rate) # The morris counter is called here 
-      test(counter, n=n, axis=axes[r][c]) 
-      # The test function is called on every respective accuracy and Failiure rate. 
+      counter = MorrisCounter(accuracy=accuracy, max_failure_rate=max_failure_rate) # Iterate across each array and make a counter instance
+      test(counter, n=n, axis=axes[r][c]) # Pass the counter instance to the test function. 
+      
 
 
-
+  # Create and Save the graph
   fig.suptitle(f"Morris(a) Counter, {n=}")
   handles, labels = axes[-1][-1].get_legend_handles_labels()
   fig.legend(handles, labels, loc='upper left', prop={'size': 6})
@@ -64,14 +96,4 @@ if __name__ == '__main__':
 
 
 
-'''
-Main function 
-The main function is responsible for taking the respective inputs namely; 
-accuracy: 
-max_failure_rate:
-bins:
-
-The function then loops over each of these respective value pairs and creates a graph for them. 
-The number of such values is defined by the number of bins we create and we then see (bins)^2 graphs for the corresponding values 
-'''
 
